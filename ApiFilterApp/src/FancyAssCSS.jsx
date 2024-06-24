f8e0c1b4def06f0287ef5e42657ced699df293d0
Ilkka Pokkinen <ilkka.pokkinen@icloud.com>
2024-06-24 15:57:29 +0300

    betaRelease

---
+++ ApiFilterApp/src/FancyAssCSS.jsx
@@ -0,0 +1,87 @@
+export const FancyAssCSS = () => {
+  return (
+    <div className="container">
+      <div className="snow"></div>
+      <div className="tree1"></div>
+      <div className="tree2"></div>
+      <div className="house">
+        <div className="roof1">
+          <div className="b1"></div>
+          <div className="b2"></div>
+        </div>
+        <div className="wall1">
+          <div className="w3">
+            <div className="window1">
+              <div className="glass1"></div>
+            </div>
+          </div>
+        </div>
+        <div className="wall2">
+          <div className="light">
+            <div className="w1">
+              <div className="window">
+                <div className="glass"></div>
+              </div>
+            </div>
+            <div className="w2">
+              <div className="window">
+                <div className="glass"></div>
+              </div>
+            </div>
+          </div>
+          <div className="door">
+            <div className="handle"></div>
+          </div>
+          <div className="snw1"></div>
+          <div className="snw2"></div>
+        </div>
+        <div className="wall3">
+          <div className="b3"></div>
+          <div className="b4"></div>
+          <div className="chimney">
+            <div className="top">
+              <div className="smoke">
+                <div className="s1"></div>
+                <div className="s2"></div>
+                <div className="s3"></div>
+              </div>
+              <div className="shne1"></div>
+              <div className="shne2"></div>
+            </div>
+          </div>
+          <div className="sn">
+            <div className="dr1"></div>
+            <div className="dr2"></div>
+            <div className="dr3"></div>
+          </div>
+          <div className="sn1">
+            <div className="dr4"></div>
+          </div>
+          <div className="sh1"></div>
+          <div className="sh2"></div>
+          <div className="sh3"></div>
+          <div className="sh4"></div>
+          <div className="sh5"></div>
+        </div>
+      </div>
+      <div className="snowfall"></div>
+      <div className="cover"></div>
+      <div className="bottom">
+        <div className="bt1"></div>
+        <div className="bt2"></div>
+      </div>
+      <div className="fence">
+        <div className="fn1">
+          <div className="screw"></div>
+        </div>
+        <div className="fn2">
+          <div className="screw"></div>
+        </div>
+        <div className="fn3">
+          <div className="screw"></div>
+        </div>
+        <div className="stck"></div>
+      </div>
+    </div>
+  );
+};
